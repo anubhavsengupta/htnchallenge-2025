c81cf8abc69183b3e1eeaa6734a0fcab33bbc002
anubhavsengupta <34662860+anubhavsengupta@users.noreply.github.com>
2025-02-12 19:45:59 -0500

    Install latest version of dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 
 export const metadata = {
   title: "Hackathon Global Inc",
-  description: "A modern Next.js application",
+  description: "HTN 2025 challenge submission",
   icons: "/logo.png",
 };
 
